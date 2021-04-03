faeca3a9ae401ef071785119f025d9885864222c
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2021-04-03 13:32:35 -0300

    fiz o countingsort normal, tudo passando nos testes. ainda nao conferi os imports e nao fiz mais casos de teste

--- src/main/java/sorting/linearSorting/CountingSort.java
+++ src/main/java/sorting/linearSorting/CountingSort.java
@@ -1,5 +1,7 @@
 package sorting.linearSorting;
 
+import java.util.Arrays;
+
 import sorting.AbstractSorting;
 
 /**
@@ -14,8 +16,106 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		boolean isSortable = false;
+		
+		//verificando se o array eh ordenavel
+		if (leftIndex < rightIndex && rightIndex < array.length) {
+			isSortable = true;
+		}
+		
+		if (!isSortable) {
+			return;
+		}
+		
+		//achando o maior elemento do array de entrada
+		int maior = array[leftIndex];
+		
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if (array[i] > maior) {
+				maior = array[i];
+			}
+		}
+		
+		Integer[] slice = Arrays.copyOfRange(array, leftIndex, rightIndex + 1);
+		Integer[] sliceOrdenado = countingSort(slice, maior);
+		int j = 0;
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			array[i] = sliceOrdenado[j];
+			j++;
+		}
+		
+	}
+	
+	private Integer[] countingSort(Integer[] slice, int k) {
+		boolean hasZero = false;
+		Integer[] sliceOrdenado = new Integer[slice.length];
+		
+		//verificando se ha elementos iguais a zero no array
+		for (int i = 0; i < slice.length; i++) {
+			if (slice[i] == 0) {
+				hasZero = true;
+			}
+		}
+		
+		if (!hasZero) {
+			sliceOrdenado = countingSortWithoutZero(slice, k);
+		} else {
+			sliceOrdenado = countingSortWithZero(slice, k);
+		}
+		
+		return sliceOrdenado;
+	}
+	
+	
+	private Integer[] countingSortWithZero(Integer[] slice, int k) {
+	//ao inves de termos que um elemento mapeado para um array de contagem cujo indice-1 representa seu valor, teremos esse
+	//mapeamento feito para um array de contagem cujo indice representa o proprio elemento. para isso o array de contagem
+	//deve ser criado do tamanho do maior elemento + 1.
+		int[] contagem = new int[k + 1];
+		
+		//passo 1: contagem da frequencia de cada elemento
+		for (int i = 0; i < slice.length; i++) {
+			contagem[slice[i]]++;
+		}
+		
+		//passo 2: soma cumulativa no array de contagem
+		for (int i = 1; i < contagem.length; i++) {
+			contagem[i] += contagem[i-1];
+		}
+		
+		//passo 3: iterar slice do fim ao inicio registrando em sliceOrdenado os elementos
+		Integer[] sliceOrdenado = new Integer[slice.length];
+		for (int i = slice.length - 1; i >= 0; i--) {
+			sliceOrdenado[contagem[slice[i]] - 1] = slice[i];
+			contagem[slice[i]]--;
+		}
+		
+		return sliceOrdenado;	
+	}
+	
+	private Integer[] countingSortWithoutZero(Integer[] slice, int k) {
+		int[] contagem = new int[k];
+		
+		//passo 1: contagem da frequencia de cada elemento
+		for (int i = 0; i < slice.length; i++) {
+			contagem[slice[i] - 1]++;
+		}
+		
+		//passo 2: soma cumulativa no array de contagem
+		for (int i = 1; i < contagem.length; i++) {
+			contagem[i] += contagem[i - 1];
+		}
+		
+		
+		
+		//passo 3: iterar slice do fim ao inicio, registrando em sliceOrdenado os elementos
+		Integer[] sliceOrdenado = new Integer[slice.length];
+		for (int i = slice.length - 1; i >= 0; i--) {
+			sliceOrdenado[contagem[slice[i] - 1] - 1] = slice[i];
+			contagem[slice[i] - 1]--;
+		}
+		
+		return sliceOrdenado;
 	}
 
 }
--- src/test/java/sorting/test/StudentSortingTest.java
+++ src/test/java/sorting/test/StudentSortingTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sorting.AbstractSorting;
+import sorting.linearSorting.CountingSort;
 
 public class StudentSortingTest {
 
@@ -38,8 +39,8 @@ public class StudentSortingTest {
 	private void getImplementation() {
 		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
 		// null
-		this.implementation = null;
-		Assert.fail("Implementation not provided");
+		this.implementation = new CountingSort();
+		//Assert.fail("Implementation not provided");
 	}
 
 	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
@@ -106,4 +107,69 @@ public class StudentSortingTest {
 	 * SEGUIR A ESTRUTURA DOS MÉTODOS DE TESTE ACIMA DESCRITOS, ORDENANDO APENAS
 	 * UMA PARTE DO ARRAY.
 	 */
+	
+	@Test
+	public void testUltimoElementoNoFinal() {
+		Integer[] array = {9, 8, 7, 6, 5, 1};
+		Integer[] arraySorted = {1, 5, 6, 7, 8, 9};
+		implementation.sort(array, 0, 5);
+		Assert.assertArrayEquals(arraySorted, array);
+	}
+	
+	@Test
+	public void testUltimoElementoNoFinalSlice() {
+		Integer[] array = {9, 8, 7, 6, 5, 1};
+		Integer[] arraySorted = {5, 6, 7, 8, 9, 1};
+		implementation.sort(array, 0, 4);
+		Assert.assertArrayEquals(arraySorted, array);
+	}
+	
+	@Test
+	public void testUltimoElementoNoFinalSlice2() {
+		Integer[] array = {9, 8, 7, 6, 5, 1};
+		Integer[] arraySorted = {9, 5, 6, 7, 8, 1};
+		implementation.sort(array, 1, 4);
+		Assert.assertArrayEquals(arraySorted, array);
+	}
+	
+	
+	@Test
+	public void testIndexInvalidos() {
+		Integer[] array = {9, 8, 7, 6, 5, 1};
+		Integer[] arrayExpected = {9, 8, 7, 6, 5, 1};
+		implementation.sort(array, 6, 9);
+		Assert.assertArrayEquals(arrayExpected, array);
+	}
+	
+	@Test
+	public void testLeftIndexInvalido() {
+		Integer[] array = {9, 8, 7, 6, 5, 1};
+		Integer[] arrayExpected = {9, 8, 7, 6, 5, 1};
+		implementation.sort(array, 6, 0);
+		Assert.assertArrayEquals(arrayExpected, array);
+	}
+	
+	@Test
+	public void testRightIndexInvalido() {
+		Integer[] array = {9, 8, 7, 6, 5, 1};
+		Integer[] arrayExpected = {9, 8, 7, 6, 5, 1};
+		implementation.sort(array, 0, 6);
+		Assert.assertArrayEquals(arrayExpected, array);
+	}
+	
+	@Test
+	public void testArrayAleatorio() {
+		Integer[] array = {32, 24, 79, 125, 20, 3, 72, 124, 475, 2303, 11};
+		Integer[] arraySorted = {3, 11, 20, 24, 32, 72, 79, 124, 125, 475, 2303};
+		implementation.sort(array, 0, 10);
+		Assert.assertArrayEquals(arraySorted, array);
+	}
+	
+	@Test
+	public void testArrayIsAlreadySorted() {
+		Integer[] array = {3, 11, 20, 24, 32, 72, 79, 124, 125, 475, 2303};
+		Integer[] arraySorted = {3, 11, 20, 24, 32, 72, 79, 124, 125, 475, 2303};
+		implementation.sort(arraySorted, 0, 10);
+		Assert.assertArrayEquals(arraySorted, array);
+	}
 }
